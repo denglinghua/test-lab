d6280c233e1deb8316e45613b5fe7135d00140b6
deng <denglinghua@outlook.com>
2020-12-17 14:24:42 +0800

    add start script

--- d.sh
+++ d.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 script_file="$1"
+repeat_times=${2:-1}
 pause_time=5 #5 seconds
 voices=(Alex Fred Samantha Victoria Karen Fiona)
 this_time_voice=Alex
@@ -15,15 +16,25 @@ function say_message() {
   say -v $this_time_voice $1
 }
 
+function print_args() {
+  echo "file  : $script_file"
+  echo "voice : $this_time_voice"
+  echo "repeat: $repeat_times"
+}
+
 set_voice
+print_args
 say_message "this is $this_time_voice"
 sleep 1
 say_message "Dictation will be start, please listen careflly"
 sleep 2
 
 while IFS= read -r line; do
+  for((i=1;i<=$repeat_times;i++))
+  do
     say_message "$line"
     sleep $pause_time
+  done
 done < $script_file
 
 say_message "The dictation is over, good_bye"
