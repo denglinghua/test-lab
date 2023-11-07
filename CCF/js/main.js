bdf0e2db9de2240cd702157edd24665ff0763145
deng <denglinghua@outlook.com>
2023-11-07 11:18:53 -0400

    CCF website init

---
+++ CCF/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+     $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop()>35)
+        {
+            $('.header').css({'background':'#FC8B01','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+        }
+        else
+        {
+            $('.header').css({'background':'none','box-shadow':'none'});
+        }
+    });
+
+   (function ($) {
+    "use strict";
+    
+})(jQuery);
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+$('.accordion-header').click(function(){
+    $('.accordion .accordion-body').slideUp(500);
+    $(this).next('.accordion-body').slideDown(500);
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');
+});
+
+});
