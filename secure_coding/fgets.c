99c5f3c3f21e517c825a0bb8e3196b583a6333ee
deng <denglinghua@outlook.com>
2024-12-13 11:44:10 -0400

    add fgets

---
+++ secure_coding/fgets.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFSIZE 32
+
+// The fgets() function keeps on reading characters until:
+// (n-1) characters have been read from the stream.
+// a newline character is encountered.
+// end of file (EOF) is reached.
+// fgets terminates at the newline character but appends it at the end of the string str. 
+// The function also appends the terminating null character at the end of the passed string.
+int read_file(const char *filename) {
+    FILE* ptr = fopen(filename, "r");
+    
+    if (ptr == NULL) {
+        fprintf(stderr, "File '%s' cannot be opened.\n", filename);
+        return -1;
+    }
+    
+    char buff[BUFSIZE];
+    
+    while (fgets(buff, BUFSIZE, ptr) != NULL) {
+        printf("%s", buff);
+    }
+    
+    fclose(ptr);
+    return 0; 
+}
+
+// This program prints the size of a specified file in bytes
+int main(int argc, char** argv) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
+    }
+
+    read_file(argv[1]);
+
+    return 0;
+}
