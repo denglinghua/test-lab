91cfb6ab525f1b772ca25f8e72ef4ff7cc87350b
deng <denglinghua@outlook.com>
2025-01-29 11:48:12 -0400

    Update stack_fuzzer.c

--- secure_coding/simlab5/stack_fuzzer.c
+++ secure_coding/simlab5/stack_fuzzer.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+// stack commands list, not including EXIT
+const char *commands[] = {
+    "ISEMPTY",
+    "ISFULL",
+    "PUSH",
+    "POP",
+    "PEEK",
+};
 
 // prints a valid sequence of at most max_number_of_commands
 // into standard output stream
 void stack_fuzzer(int max_number_of_commands) {
-    return;
+    for (int i = 0; i < max_number_of_commands; i++) {
+        int command_index = rand() % (sizeof(commands) / sizeof(commands[0]));
+        const char *command = commands[command_index];
+        printf("%s\n", command);
+        // if the command is PUSH, append a random number
+        if (strcmp(command, "PUSH") == 0) {
+            printf("%d\n", rand());
+        }
+    }
+    printf("EXIT\n");
 }
 
 int main() {
     // use current time as a seed for the pseudorandom
     // number generator
     srand(time(NULL));
-    stack_fuzzer(10);
+    stack_fuzzer(20);
 }
