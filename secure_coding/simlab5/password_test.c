006a4a7790fa29ed5c62e7b2ccc09803ad6e6ed6
deng <denglinghua@outlook.com>
2025-01-29 22:47:46 -0400

    Exercise 3

--- secure_coding/simlab5/password_test.c
+++ secure_coding/simlab5/password_test.c
@@ -5,7 +5,20 @@
 #define FALSE 0
 
 int main() {
-    // this test checks if short passwords are not accepted
-    assert(is_valid_password("s3cURe?") == FALSE);
-    // add more assertion-based tests here
+    assert(is_valid_password("length7") == FALSE); // too short
+    assert(is_valid_password("length8!") == FALSE); // missing uppercase letter
+    assert(is_valid_password("LENGTH8!") == FALSE); // missing lowercase letter
+    assert(is_valid_password("Length!!") == FALSE); // missing digit
+    assert(is_valid_password("Length88") == FALSE); // missing special character
+
+    assert(is_valid_password("Length8!") == TRUE); // valid
+
+    assert(is_valid_password("A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!50") == TRUE); // 50 characters
+    assert(is_valid_password("A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A51") == FALSE); // 51 characters
 }
+
+// run result: 
+// Assertion failed: (is_valid_password("A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A1b2c3d!A51") == FALSE), function main, file password_test.c, line 17.
+
+// Conclusion: 
+// the function is_valid_password() does not check the maximum length of the password.
